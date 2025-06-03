05facf8f46f7e5f0cfa1027321d8c4cc417033b6
Perdi <perdi@fungsitama.com>
2025-06-03 15:14:45 +0700

    CI/CD

--- lib/data/datasource/local_datasource.dart
+++ lib/data/datasource/local_datasource.dart
@@ -6,49 +6,40 @@ class LocalDataSource {
   static const String _emailKey = 'savedEmail';
   static const String _passwordKey = 'savedPassword';
 
-  static Box? _box;
+  late Box _box;
 
-  Future<Box> _getBox() async {
-    if (_box == null || !_box!.isOpen) {
-      _box = await Hive.openBox(_authBox);
-    }
-    return _box!;
+  // Inisialisasi, harus dipanggil sekali sebelum menggunakan
+  Future<void> init() async {
+    _box = await Hive.openBox(_authBox);
   }
 
   Future<void> saveToken(String token) async {
-    final box = await _getBox();
-    await box.put(_tokenKey, token);
+    await _box.put(_tokenKey, token);
   }
 
   Future<String?> getToken() async {
-    final box = await _getBox();
-    return box.get(_tokenKey);
+    return _box.get(_tokenKey);
   }
 
   Future<void> clearToken() async {
-    final box = await _getBox();
-    await box.delete(_tokenKey);
+    await _box.delete(_tokenKey);
   }
 
   Future<void> saveCredentials(String email, String password) async {
-    final box = await _getBox();
-    await box.put(_emailKey, email);
-    await box.put(_passwordKey, password);
+    await _box.put(_emailKey, email);
+    await _box.put(_passwordKey, password);
   }
 
   Future<String?> getEmail() async {
-    final box = await _getBox();
-    return box.get(_emailKey);
+    return _box.get(_emailKey);
   }
 
   Future<String?> getPassword() async {
-    final box = await _getBox();
-    return box.get(_passwordKey);
+    return _box.get(_passwordKey);
   }
 
   Future<void> clearCredentials() async {
-    final box = await _getBox();
-    await box.delete(_emailKey);
-    await box.delete(_passwordKey);
+    await _box.delete(_emailKey);
+    await _box.delete(_passwordKey);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,77 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
-import 'presentation/pages/Home/home_page.dart';
-import 'presentation/pages/on_boarding/on_boarding.dart';
-
-import 'bloc/audio/audio_bloc.dart';
+import 'bloc/login/login_bloc.dart';
 import 'bloc/course/course_bloc.dart';
 import 'bloc/course_detail/course_detail_bloc.dart';
+import 'bloc/audio/audio_bloc.dart';
 import 'bloc/lesson/lesson_bloc.dart';
-import 'bloc/login/login_bloc.dart';
-
-import 'data/datasource/audio_datasource.dart';
 import 'data/datasource/auth_datasource.dart';
 import 'data/datasource/course_datasource.dart';
+import 'data/datasource/audio_datasource.dart';
 import 'data/datasource/lesson_datasource.dart';
 import 'data/datasource/local_datasource.dart';
+import 'presentation/pages/Home/home_page.dart';
+import 'presentation/pages/on_boarding/on_boarding.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
 
-  runApp(const MyApp());
+  // Inisialisasi LocalDataSource dan buka box sekali saja
+  final localDataSource = LocalDataSource();
+  await localDataSource.init();
+
+  String? token = await localDataSource.getToken();
+
+  runApp(MyApp(token: token));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  Future<bool> _checkLoginStatus() async {
-    final token = await LocalDataSource().getToken();
-    return token != null;
-  }
+  final String? token;
+  const MyApp({super.key, required this.token});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: _checkLoginStatus(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const MaterialApp(
-            home: Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            ),
-          );
-        }
-
-        final isLoggedIn = snapshot.data ?? false;
-
-        return MultiBlocProvider(
-          providers: [
-            BlocProvider(create: (_) => LoginBloc(AuthDataSource())),
-            BlocProvider(create: (_) => CourseBloc(CourseDataSource())),
-            BlocProvider(create: (_) => CourseDetailBloc(CourseDataSource())),
-            BlocProvider(create: (_) => AudioBloc(AudioDataSource())),
-            BlocProvider(create: (_) => LessonBloc(LessonDataSource())),
-          ],
-          child: ScreenUtilInit(
-            designSize: const Size(375, 812),
-            minTextAdapt: true,
-            splitScreenMode: true,
-            builder: (context, child) {
-              return MaterialApp(
-                debugShowCheckedModeBanner: false,
-                theme: ThemeData(
-                  useMaterial3: true,
-                  colorSchemeSeed: Colors.blue,
-                ),
-                home: isLoggedIn ? const HomePage() : const OnBoarding(),
-              );
-            },
-          ),
-        );
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => LoginBloc(AuthDataSource())),
+        BlocProvider(create: (context) => CourseBloc(CourseDataSource())),
+        BlocProvider(create: (context) => CourseDetailBloc(CourseDataSource())),
+        BlocProvider(create: (context) => AudioBloc(AudioDataSource())),
+        BlocProvider(create: (context) => LessonBloc(LessonDataSource())),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: token == null ? const OnBoarding() : const HomePage(),
+        ),
+      ),
     );
   }
 }
